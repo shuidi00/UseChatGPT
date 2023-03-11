26119bfd47aefcc4ba265a786e9408e025d556d8
shuidi00 <shuidi00@foxmail.com>
2023-03-11 19:46:44 +0800

    1.0

---
+++ ChatAI.py
@@ -0,0 +1,74 @@
+"""
+此程序用于调用ChatGPT接口
+"""
+import openai
+import logging
+
+# 创建一个日志记录器
+logger = logging.getLogger('my_logger')
+# 设置日志级别
+logger.setLevel(logging.INFO)
+# 创建一个FileHandler，将日志写入文件中
+file_handler = logging.FileHandler('ai.log', encoding='utf-8')
+# 创建一个Formatter，用于指定日志输出格式
+formatter = logging.Formatter('%(message)s')
+# 将Formatter添加到FileHandler中
+file_handler.setFormatter(formatter)
+# 将FileHandler添加到日志记录器
+logger.addHandler(file_handler)
+
+
+openai.api_key = "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"
+last_question = ''
+last_answer = ''
+
+
+def request_ai(this_q, last_q, last_a):
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "你是一个人。"},
+            {"role": "user", "content": last_q},
+            {"role": "assistant", "content": last_a},
+            {"role": "user", "content": this_q}
+        ],
+        timeout=60
+    )
+    result = ''
+    for choice in response.choices:
+        result += choice.message.content
+    return result
+
+
+def ask():
+    while True:
+        try:
+            global last_question
+            global last_answer
+            question = input('问：')
+            logger.info('问：')
+            logger.info(question)
+            answer = request_ai(question, last_question, last_answer)
+            print('答：', end='')
+            print(answer)
+            logger.info('答：')
+            logger.info(answer)
+            last_question, last_answer = question, answer
+            if '谢谢' in question or '谢谢' in answer or '感谢' in answer:
+                print('如果有帮到您，可访问以下地址给予支持，谢谢。')
+                print('https://www.kdocs.cn/l/cpLMSSf0lXD5')
+                logger.info('如果有帮到您，可访问以下地址给予支持，谢谢。')
+                logger.info('https://www.kdocs.cn/l/cpLMSSf0lXD5')
+        except Exception as e:
+            logger.debug(e)
+            print('答：', end='')
+            print('网络不稳定，请再试一次')
+            logger.info('答：')
+            logger.info('网络不稳定，请再试一次')
+        finally:
+            print('-' * 60)
+            logger.info('-' * 60)
+
+
+if __name__ == '__main__':
+    ask()
--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # UseChatGPT
 记录ChatGPT的使用，如e用接口，exe封装，有价值的回答等
+
+## 如何将python文件打包成exe程序
+
+Python文件可以使用Pyinstaller等工具将其编译成exe程序。以下是使用Pyinstaller的步骤：
+
+1. 安装Pyinstaller：可在终端中使用pip install pyinstaller命令进行安装。
+
+2. 打开终端并导航到Python文件所在目录。
+
+3. 在终端中使用以下命令将Python文件编译成exe程序：
+
+   ```
+   pyinstaller --onefile yourscript.py
+   ```
+
+   上述命令将生成一个单文件的exe程序。
+
+4. 编译完成后，在dist文件夹下可以找到生成的exe程序。
+
+注意事项：
+
+- 如果Python文件中有依赖包，则需要在编译时添加参数。例如：
+
+  ```
+  pyinstaller --onefile --hidden-import=pandas yourscript.py
+  ```
+
+- 编译时需要特别注意Python文件中的路径设置，需要使用绝对路径而不是相对路径，否则可能导致生成的exe程序无法正常运行。
+
+ 使用 PyInstaller 打包应用程序时，需要添加 `--add-data` 参数来指定需要打包的文件。例如，可以使用以下命令打包应用程序：
+
+   ```bash
+   pyinstaller --add-data="images/*.png:images" main.py
+   ```
+
+   上面的命令将会将 `images` 文件夹下的所有 `.png` 图片文件打包进可执行文件中。
+
+使用python3.10
+pyinstaller --onefile --add-binary="picture;picture"  --hidden-import=openai --hidden-import=Pillow  ai.py
+pyinstaller --onefile --icon=icon.ico  --hidden-import=openai --hidden-import=Pillow  ai.py
